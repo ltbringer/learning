4310cc8527d9a3febd9b6af02807a4bf63221dcd
ltbringer <amresh.venugopal@gmail.com>
2021-06-18 04:01:10 +0530

    ch4: 4.5.2 use fewer long lived variables.

--- programming/rust/rust-in-action/ch4_cubesats-and-ownership/src/main.rs
+++ programming/rust/rust-in-action/ch4_cubesats-and-ownership/src/main.rs
@@ -1,17 +1,27 @@
 #[derive(Debug)]
-enum StatusMessage {
-    Ok,
+struct Message {
+    to: u64,
+    content: String,
 }
-
-fn check_status(sat_id: CubeSat) -> StatusMessage {
-    StatusMessage::Ok
-}
-
-type Message = String;
 
 #[derive(Debug)]
 struct Mailbox {
     messages: Vec<Message>,
+}
+
+impl Mailbox {
+    fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                return Some(self.messages.remove(i));
+            }
+        }
+        None
+    }
+
+    fn post(&mut self, message: Message) {
+        self.messages.push(message);
+    }
 }
 
 #[derive(Debug)]
@@ -21,41 +31,52 @@ struct CubeSat {
 }
 
 impl CubeSat {
-    fn recv(&mut self) -> Option<Message> {
-        self.mailbox.messages.pop()
+    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
+        mailbox.deliver(&self)
     }
+}
+
+fn fetch_sat_ids() -> Vec<u64> {
+    vec![1, 2, 3]
 }
 
 #[derive(Debug)]
 struct GroundStation {}
 
 impl GroundStation {
-    fn send(&self, to: &mut CubeSat, msg: Message) {
-        to.mailbox.messages.push(msg);
+    fn send(&self, mailbox: &mut Mailbox, msg: Message) {
+        mailbox.post(msg);
+    }
+
+    fn connect(&self, sat_id: u64) -> CubeSat {
+        CubeSat {
+            id: sat_id,
+            mailbox: Mailbox { messages: vec![] },
+        }
     }
 }
 
 fn main() {
     let base = GroundStation {};
-    let mut sat_a = CubeSat {
-        id: 0,
-        mailbox: Mailbox { messages: vec![] },
-    };
-    let sat_b = CubeSat {
-        id: 0,
-        mailbox: Mailbox { messages: vec![] },
-    };
-    let sat_c = CubeSat {
-        id: 0,
-        mailbox: Mailbox { messages: vec![] },
-    };
+    let mut mailbox = Mailbox { messages: vec![] };
+    let sat_ids = fetch_sat_ids();
 
-    println!("t0: {:?}", sat_a);
-    base.send(&mut sat_a, Message::from("PING"));
+    for sat_id in sat_ids {
+        let msg = Message {
+            to: sat_id,
+            content: String::from("hello"),
+        };
+        base.send(&mut mailbox, msg)
+    }
 
-    println!("t1: {:?}", sat_a);
-    let msg = sat_a.recv();
+    let sat_ids = fetch_sat_ids();
 
-    println!("t2: {:?}", sat_a);
-    println!("message: {:?}", msg);
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = sat.recv(&mut mailbox);
+        match msg {
+            Some(data) => println!("sat_id {} 👂 '{}'", sat.id, data.content),
+            None => println!("sat_id {}: 👂 '{}'", sat.id, "NA"),
+        }
+    }
 }
