30f7b59e18a0952fbd983d3715e0b34f7aed5b87
ltbringer <amresh.venugopal@gmail.com>
2021-09-25 20:01:14 +0530

    update: timer function gets logger and functools.wrap.

--- programming/python/concurrency/concurrency/utils/__init__.py
+++ programming/python/concurrency/concurrency/utils/__init__.py
@@ -1,12 +1,15 @@
 import time
+import functools
+from concurrency.utils.logger import logger
 
 
 def t(fn):
+    @functools.wraps(fn)
     def wrapper(*args, **kwargs):
         start = time.process_time()
         result = fn(*args, **kwargs)
         end = time.process_time()
-        print(
+        logger.debug(
             f"\n▫️{fn.__name__} took {(end - start):.3f} seconds -- returns {result}."
         )
         return result
