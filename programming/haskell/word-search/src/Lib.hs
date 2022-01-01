4f79faac2c69bd086761cada06221ada2514f9dc
ltbringer <amresh.venugopal@gmail.com>
2022-01-02 00:39:32 +0530

    refactor: move out data from lib.

--- programming/haskell/word-search/src/Lib.hs
+++ programming/haskell/word-search/src/Lib.hs
@@ -1,41 +1,18 @@
 module Lib (
-    grid,
-    languages,
-    formatGrid,
     outputGrid,
-    findWord
+    formatGrid,
+    findWordInLine,
+    getLines,
+    skewRight,
+    skewLeft,
+    findWord,
+    findWords,
 ) where
 
-import Data.List (isInfixOf)
+import Data.List (isInfixOf, transpose, intercalate)
 import Data.Maybe (catMaybes)
 
 type Grid = [String]
-
-grid = [ "__C________R___"
-       , "__SI________U__"
-       , "__HASKELL____B_"
-       , "__A__A_____S__Y"
-       , "__R___B___C____"
-       , "__PHP____H_____"
-       , "____S_LREP_____"
-       , "____I__M_Y__L__"
-       , "____L_E__T_O___"
-       , "_________HB____"
-       , "_________O_____"
-       , "________CN_____"
-       ]
-
-languages = [ "BASIC"
-            , "COBOL"
-            , "CSHARP"
-            , "HASKELL"
-            , "LISP"
-            , "PERL"
-            , "PHP"
-            , "PYTHON"
-            , "RUBY"
-            , "SCHEME"
-            ]
 
 -- | Display the grid.
 outputGrid :: Grid -> IO ()
@@ -44,17 +21,39 @@ outputGrid grid = putStrLn $ formatGrid grid
 
 -- | Format the grid.
 formatGrid :: Grid -> String
-formatGrid = unlines
+formatGrid = intercalate "\n"
 
 findWordInLine :: String -> String -> Bool
 findWordInLine = isInfixOf
 
+
+getLines :: Grid -> [String]
+getLines grid = 
+    let horizontal = grid
+        vertical = transpose horizontal
+        diagonal1 = (transpose . skewLeft) grid
+        diagonal2 = (transpose . skewRight) grid
+        lines = horizontal ++ vertical ++ diagonal1 ++ diagonal2
+    in lines ++ (map reverse lines)
+
+
+skewRight :: Grid -> Grid
+skewRight [] = []
+skewRight (x:xs) = x : skewRight (map ((++) "_") xs)
+    where indent line = '_' : line
+
+
+skewLeft :: Grid -> Grid
+skewLeft grid = skewRight (map reverse grid)
+
+
 -- | Find a word in the grid.
 findWord :: Grid -> String -> Maybe String
 findWord grid word =
-    let lines = grid ++ (map reverse grid)
+    let lines = getLines grid
         match = or $ map (findWordInLine word) lines
     in if match then Just word else Nothing
 
-findLanguage :: Grid -> [String] -> [String]
-findLanguage grid languages = catMaybes $ map (\language -> (findWord grid language)) languages
+
+findWords :: Grid -> [String] -> [String]
+findWords grid languages = catMaybes $ map (\language -> (findWord grid language)) languages
