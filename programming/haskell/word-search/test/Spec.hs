728f3d8a4dfd1db08a88cc259e7f75842b391986
ltbringer <amresh.venugopal@gmail.com>
2022-01-02 00:40:34 +0530

    test: findWords for searching words within grid.

--- programming/haskell/word-search/test/Spec.hs
+++ programming/haskell/word-search/test/Spec.hs
@@ -1,2 +1,14 @@
+import Test.Hspec
+import Data
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "formatGrid" $ do
+        it "Should concatenate everyline with a \\n." $ do
+            (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi"
+
+    describe "findWord" $ do
+        it "Should return the word if it is in the grid." $ do
+            (findWord grid "hello") `shouldBe` Nothing
+            (findWord grid "HASKELL") `shouldBe` Just "HASKELL"
