a196124e29c808809a7679a35a61551866d4e75b
ltbringer <amresh.venugopal@gmail.com>
2022-02-17 03:43:55 +0530

    add: implement player.

--- programming/rust/gamedev/flappy/src/main.rs
+++ programming/rust/gamedev/flappy/src/main.rs
@@ -15,6 +15,35 @@ struct Player {
     velocity: f32
 }
 
+impl Player {
+    fn new(x: i32, y: i32) -> Player {
+        Player {
+            x,
+            y,
+            velocity: 0.0
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn gravity_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity  += 0.2
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 struct State {
     player: Player,
     frame_time: f32,
