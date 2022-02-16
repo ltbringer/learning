adb4468fdeb69b988ed916600edabd8c50587f5b
ltbringer <amresh.venugopal@gmail.com>
2022-02-17 03:42:51 +0530

    add: game mode, player and game state.

--- programming/rust/gamedev/flappy/src/main.rs
+++ programming/rust/gamedev/flappy/src/main.rs
@@ -1,5 +1,23 @@
 use bracket_lib::prelude::*;
 
+enum GameMode {
+    Menu,
+    Playing,
+    End
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32
+}
+
+struct State {
+    player: Player,
+    frame_time: f32,
+    mode: GameMode,
+}
+
 
 fn main() -> BError {
     let ctx = BTermBuilder::simple80x50()
